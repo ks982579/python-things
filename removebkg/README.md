daee0995ae0a1e63b3043cd210a943383162282f
ks982579 <kevinsullivan@ksulldev.space>
2023-05-27 13:46:40 +0100

    Updates to a functioning project

--- removebkg/README.md
+++ removebkg/README.md
@@ -5,3 +5,8 @@ requirements:
 pip install --upgrade Pillow
 pip install rembg easygui
 ```
+
+When saving the file, save it as a `*.png` to preserve the background removal.
+It will take a minute to fire up and it should download some "u2net.onnx".
+
+There are other ways to use the `rembg` package, see the [github README](https://github.com/danielgatis/rembg).
--- removebkg/main.py
+++ removebkg/main.py
@@ -1,8 +1,19 @@
 """
-Thanks to https://www.tomshardware.com/how-to/python-remove-image-backgrounds
-
-pip install --upgrade Pillow
-pip install rembg easygui
-
+Small script to (hopefully) remove background from images for Free!
 """
 
+from PIL import Image
+from rembg import remove
+import easygui as eg
+
+# Object to store path and name of file
+input_path = eg.fileopenbox(title="Select Image File")
+
+# Object to store path to save file
+output_path = eg.filesavebox(title="Save File to...")
+
+image_with_background = Image.open(input_path)
+image_without_background = remove(image_with_background)
+
+# Set format to PNG when saving
+image_without_background.save(output_path)
